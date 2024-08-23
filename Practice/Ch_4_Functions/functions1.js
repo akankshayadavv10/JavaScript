f96297d9f24fc44961dfa16841d3eead0695121d
akankshayadavv10 <115527875+akankshayadavv10@users.noreply.github.com>
2024-08-23 14:40:50 +0000

    23/8/2024

---
+++ Practice/Ch_4_Functions/functions1.js
@@ -0,0 +1,15 @@
+// function greet(){
+//     alert("Hey,Good AfterNoon!")
+// }
+// greet();
+
+
+function multiply(a,b){
+    console.log(a*b)
+}
+multiply(10,30);
+
+function square(n){
+    return(n*n)
+}
+console.log("the square of the number is:",square(4));
