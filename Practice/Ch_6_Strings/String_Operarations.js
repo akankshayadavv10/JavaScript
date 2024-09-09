84b4cfb3abe46703c487e44ab7495a50303b4c0b
akankshayadavv10 <115527875+akankshayadavv10@users.noreply.github.com>
2024-09-09 09:56:20 +0000

    9/9/2024

---
+++ Practice/Ch_6_Strings/String_Operarations.js
@@ -0,0 +1,36 @@
+// length
+let str1="You are welcome to the coding surface!!";
+console.log(str1.length);
+// 2
+let str2="Hello";
+console.log(str2.length);
+
+
+// String Concat(Merging sttrings)
+let str3="Hi";
+let str4="Hii";
+console.log(str3+str4);
+// 2
+let str5="bye";
+let str6='hiii';
+console.log(str5+str6);
+
+
+// Escape Char(single quotes,double quotes,backslash(\' - Inserts a single quote  || \" - Inserts a double quote || \\ - Inserts a backslash))
+
+// breaking long string
+let str7=("You are welcome to the \  coding space");
+console.log(str7);
+// 2
+let str8=("hi \ i hope you \ are doing good");
+console.log(str8);
+
+
+// Substring(substr)
+let str9="javaScript Tutorial";
+let substr=str9.substr(0,10);
+console.log(substr);
+// 2
+console.log(str9);
+let substr1=str9.substr(1,6);
+console.log(substr1);
---
+++ Practice/Ch_6_Strings/Strings.js
@@ -0,0 +1,58 @@
+// Using Single Quotation
+let str='Hello Akanksha Welcome to JavaScript!';
+console.log(str);
+// 2
+let str1='Hello Akku welcome to JavaScript';
+console.log(str1);
+
+
+// Using Double Quotation
+let str2="Hello Coder!";
+console.log(str2);
+// 2
+let str3="Hello There any problem?";
+console.log(str3);
+
+
+// String Constructor(new Keyword)
+let str4=new String("Create a string with a constructor");
+console.log(str4);
+// 2
+let str5=new String('Honesty is the best policy');
+console.log(str5);
+
+
+// Template literals(embed expressions within backticks(`))
+let str6='Akanksha';
+let newstr=`Her name is ${str6}`;
+console.log(newstr);
+// 2
+let str7='You are welcome to';
+let newstr2=`${str7} Disha`;
+console.log(newstr2);
+
+
+// Empty String(no characters)
+let str8='';
+console.log(str8);
+// 2
+let str9="";
+console.log(str9);
+
+
+// Multiline String(uses backticks)
+let stra=`This is very
+
+first 
+program of 
+String....`;
+console.log(stra);
+// 2
+let strb=`Hello
+is anyone
+            there
+?`;
+console.log(strb);
+
+
+
