da102ffe33839f844821d2b2f5a81ae361c990ca
akankshayadavv10 <115527875+akankshayadavv10@users.noreply.github.com>
2024-09-13 09:02:19 +0000

    13/09/2024

--- Practice/Ch_7_Numbers/Numbers.js
+++ Practice/Ch_7_Numbers/Numbers.js
@@ -22,3 +22,75 @@ let x = 0.22 + 0.12;
 let y = (0.22 * 10 + 0.11 * 10) / 10;
 console.log(x);
 console.log(y);
+
+
+
+
+// Adding no. and stringss
+let e=10;
+let f=11;
+console.log(e+f);
+// 2
+let g=12;
+let h="Akkku";
+console.log(g+h);
+// 3
+let i="Akanksha";
+let j="Akku";
+console.log(i+j);
+
+
+
+
+// Numeric Strings(converts numeric strings into numbers)
+let k="10"*"10"
+let l="10"/"10";
+console.log(k);
+console.log(l);
+
+
+
+
+// *Numberv Literals
+// 1(Decimal Numbers)
+let m=10.1;
+console.log(m);
+
+// 2(Octal Numbwrs( starts with 0 and the following number is smaller than 8))
+let n = 0562;
+console.log(n);
+
+// 3(Binary Numbers(Starts with 0b or 0B followed  by 0 and 1))
+let o=0b11;
+let p=0B0111;
+console.log(o);
+console.log(p);
+
+// 4(Hexadecimal(Starts with 0x or 0X))
+let q=0xfff;
+console.log(q);
+
+
+
+//*Number Coersion
+// 1(NAN-Not A Number(Involves undefined ))
+let r=undefined+10;
+console.log(r);
+
+// 2(Null to 0(null values is supposed to be 0))
+let s=null+2;
+console.log(s);
+
+// 3(Boolean to number(Converts the numbers:1for true and 0 for false))
+let t=true+1;
+let u=false-11;
+console.log(t);
+console.log(u);
+
+// 4(String to Number())
+let v='42';
+let w='Hello';
+const numFromString1 = Number(v);
+const numFromString2 = Number(w);
+console.log(v);
+console.log(w);
