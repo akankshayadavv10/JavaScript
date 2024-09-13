fe3b7c6a671cd151310da950b1771e510d724616
akankshayadavv10 <115527875+akankshayadavv10@users.noreply.github.com>
2024-09-13 09:49:47 +0000

    13/9/2024

---
+++ Practice/Ch_8_Math/properties.js
@@ -0,0 +1,62 @@
+console.log(Math.PI);
+
+console.log(Math.E);
+
+console.log(Math.random()*10);
+
+
+
+
+
+
+// *Math Methods
+// 1(abs-returns absolute vlues i.e positive values)
+console.log(Math.abs(-10));
+// 
+let a=2.9;
+let b=-80;
+let res=a+b;
+console.log(Math.abs(res));
+
+// 2(cbrt-returns the cube root of number)
+console.log(Math.cbrt(9));
+// 
+console.log(Math.cbrt(a+b));
+
+// 3(ceil-rounding off to the bigger number)
+console.log(Math.ceil(a));
+// 
+let c=-4.7
+console.log(Math.ceil(c));
+
+// 4(floor-opp. of ceil)
+console.log(Math.floor(c))
+// 
+let d=3.1;
+console.log(Math.floor(d));
+
+// 5(fround-rounding off)
+console.log(Math.round(d));
+// 
+let e=2.89;
+console.log(Math.round(e));
+
+// 6(max-find the maximum number)
+let f=22;
+let g=56;
+let h=78;
+console.log(Math.max(f,g,h));
+// 
+console.log(Math.max(12,45,90));
+
+// 7(min-find the minimum from the numbers)
+console.log(Math.min(f,g,h));
+// 
+console.log(Math.min(12,34,56));
+
+// 8(pow-power)
+console.log(Math.pow(10,2));
+
+// 9(trunc-removes the fractional part)
+console.log(Math.trunc(15.56));
+
