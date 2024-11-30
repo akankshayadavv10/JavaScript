8b023336c833d7abe3b3499fb6ce4f910d6229c3
akankshayadavv10 <115527875+akankshayadavv10@users.noreply.github.com>
2024-11-30 08:39:44 +0000

    Object_Method Implemented

--- Practice/Ch_11_Object/Object_Methods/Objectvalues.js
+++ Practice/Ch_11_Object/Object_Methods/Objectvalues.js
@@ -6,3 +6,13 @@ let person={
 }
 let txt=Object.values(person);
 console.log(txt);
+
+// 2
+let Student={
+    firstName:"Akanksha",
+    lastName:"Yadav",
+    rollNo:20,
+
+}
+let txt1=Object.values(student);
+console.log(txt1);
